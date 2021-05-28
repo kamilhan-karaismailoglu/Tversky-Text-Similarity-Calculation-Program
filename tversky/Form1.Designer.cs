687587bfd795f03ac6188bf22c3bfe56eaa0fe35
Kamilhan KARAİSMAİLOĞLU <k_karaismailoglu@aof.anadolu.edu.tr>
2021-05-29 01:39:24 +0300

    Proje dosyası ekle.

---
+++ tversky/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace tversky
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textbox2 = new System.Windows.Forms.TextBox();
+            this.textbox1 = new System.Windows.Forms.TextBox();
+            this.hesaplama_türü = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(359, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 38);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(112, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(264, 18);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "METİN BENZERLİĞİ HESAPLAMA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.label4.Location = new System.Drawing.Point(312, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 15);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "2. METNİN AĞIRLIĞI  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.label3.Location = new System.Drawing.Point(312, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 15);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "1. METNİN AĞIRLIĞI  :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.label2.Location = new System.Drawing.Point(16, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "2. METİN  :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.label1.Location = new System.Drawing.Point(16, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "1. METİN  :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(452, 149);
+            this.textBox4.MaxLength = 5;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(56, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(452, 85);
+            this.textBox3.MaxLength = 5;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(56, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textbox2
+            // 
+            this.textbox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textbox2.Location = new System.Drawing.Point(94, 146);
+            this.textbox2.Multiline = true;
+            this.textbox2.Name = "textbox2";
+            this.textbox2.Size = new System.Drawing.Size(190, 39);
+            this.textbox2.TabIndex = 13;
+            // 
+            // textbox1
+            // 
+            this.textbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textbox1.Location = new System.Drawing.Point(94, 82);
+            this.textbox1.Multiline = true;
+            this.textbox1.Name = "textbox1";
+            this.textbox1.Size = new System.Drawing.Size(190, 39);
+            this.textbox1.TabIndex = 12;
+            // 
+            // hesaplama_türü
+            // 
+            this.hesaplama_türü.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.hesaplama_türü.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.hesaplama_türü.CheckOnClick = true;
+            this.hesaplama_türü.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.hesaplama_türü.FormattingEnabled = true;
+            this.hesaplama_türü.Items.AddRange(new object[] {
+            "Tversky İndexi ile Hesapla",
+            "Jaccard İndexi ile Hesapla",
+            "Sørensen–Dice İndexi ile Hesapla"});
+            this.hesaplama_türü.Location = new System.Drawing.Point(31, 219);
+            this.hesaplama_türü.Name = "hesaplama_türü";
+            this.hesaplama_türü.Size = new System.Drawing.Size(265, 51);
+            this.hesaplama_türü.TabIndex = 22;
+            this.hesaplama_türü.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(534, 293);
+            this.Controls.Add(this.hesaplama_türü);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textbox2);
+            this.Controls.Add(this.textbox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Metin Benzerliği Hesaplama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textbox2;
+        private System.Windows.Forms.TextBox textbox1;
+        private System.Windows.Forms.CheckedListBox hesaplama_türü;
+    }
+}
+
---
+++ tversky/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tversky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            for (int i = 0; i < hesaplama_türü.Items.Count; i++) // Yanlızca bir checkboxı seçilir yapar
+            {
+                if (i != e.Index)
+                    hesaplama_türü.SetItemChecked(i, false);
+            }
+
+            if (hesaplama_türü.SelectedIndex == 1 || hesaplama_türü.SelectedIndex == 2)  // 2. ve 3. checkbox seçili ise
+            {
+                textBox3.Enabled = false;   // 3. textbox kapatılır
+                textBox4.Enabled = false;   // 4. textbox kapatılır
+            }
+            else
+            {
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( hesaplama_türü.SelectedIndex == -1)     // arama türünü seçilmesini sorgular
+            {
+                MessageBox.Show("Lütfen Hesaplama Türünü Seçiniz");
+            }
+            else
+            {
+                if (hesaplama_türü.SelectedIndex == 1)
+                {
+                    textBox3.Text = 1.ToString();
+                    textBox4.Text = 1.ToString();
+                }
+                if (hesaplama_türü.SelectedIndex == 2)
+                {
+                    textBox3.Text = (0.5).ToString();
+                    textBox4.Text = (0.5).ToString();
+                }
+                if (textbox1.Text == "" || textbox2.Text == "" || textbox1.Text == null || textbox2.Text == null ||
+                    textbox1.Text.Length <2 || textbox2.Text.Length < 2 ||
+                    textBox3.Text == "" || textBox4.Text == "" || textBox3.Text == null || textBox4.Text == null)
+                {
+                    if (textbox1.Text.Length < 2 || textbox2.Text.Length < 2 )
+                    {
+                        MessageBox.Show("Girilen Metinler En Az 2 Karakterli Olmalıdır");
+                    }
+                    if (textBox3.Text == "" || textBox4.Text == "" || textBox3.Text == null || textBox4.Text == null)
+                    {
+                        MessageBox.Show("Lütfen Metin Ağırlıklarını Giriniz");
+                    }
+                }
+                else
+                {
+                    string metin1 = textbox1.Text;
+                    string metin2 = textbox2.Text;
+
+                    List<string> bigram1 = new List<string>();  // bigramlar için liste oluşturur
+                    List<string> bigram2 = new List<string>();
+
+                    for (int i = 1; i < metin1.Length; i++)     // 1. metni bigrama çevirir ve bigram1 listesine ekler
+                    {
+                        if (metin1[i].ToString() != " " && bigram1.Contains(metin1[i - 1].ToString() + metin1[i].ToString()) == false)
+                        {
+                            bigram1.Add(metin1[i - 1].ToString() + metin1[i].ToString());
+                        }
+                        else if (metin1[i].ToString() == " " && bigram1.Contains("_" + metin1[i + 1].ToString()) == false && bigram1.Contains(metin1[i - 1].ToString() + "_") == false)
+                        {
+                            bigram1.Add(metin1[i - 1].ToString() + "_");
+                            bigram1.Add("_" + metin1[i + 1].ToString());
+                            i++;
+                        }
+                    }
+
+                    for (int i = 1; i < metin2.Length; i++)      // 2. metni bigrama çevirir ve bigram2 listesine ekler
+                    {
+                        if (metin2[i].ToString() != " " && bigram2.Contains(metin2[i - 1].ToString() + metin2[i].ToString()) == false) // metin 2
+                        {
+                            bigram2.Add(metin2[i - 1].ToString() + metin2[i].ToString());
+                        }
+                        else if (metin2[i].ToString() == " " && bigram2.Contains("_" + metin2[i + 1].ToString()) == false && bigram2.Contains(metin2[i - 1].ToString() + "_") == false)
+                        {
+                            bigram2.Add(metin2[i - 1].ToString() + "_");
+                            bigram2.Add("_" + metin2[i + 1].ToString());
+                            i++;
+                        }
+                    }
+
+                    List<string> kesisim = new List<string>();      // kesişim kümesi için liste oluşturur
+
+                    foreach (var item in bigram1)   // aynı elemanları sorgular ve kesişim listesine ekler
+                    {
+                        foreach (var item2 in bigram2)
+                        {
+                            if (item == item2)
+                            {
+                                kesisim.Add(item);
+                            }
+                        }
+                    }
+
+                    var fark1 = bigram1.Except(bigram2).ToList();   // bigram1 listesinin bigram2 listesinden farkını fark1 listesine ekler
+                    var fark2 = bigram2.Except(bigram1).ToList();    // bigram2 listesinin bigram1 listesinden farkını fark2 listesine ekler
+
+                    int kesisimsayı = kesisim.Count;    // kesişim kümesinin eleman sayısı
+                    int f1 = fark1.Count;               // bigram1 fark bigram2 kümesinin eleman sayısı
+                    int f2 = fark2.Count;               // bigram2 fark bigram1 kümesinin eleman sayısı
+
+                    double katsayı1;
+                    double katsayı2;
+
+                    if (hesaplama_türü.SelectedIndex == 0)     // hesaplama türü tversky seçilirse
+                    {
+                        katsayı1 = Convert.ToDouble(textBox3.Text);
+                        katsayı2 = Convert.ToDouble(textBox4.Text);
+                    }
+                    else if (hesaplama_türü.SelectedIndex == 1) // hesaplama türü jaccard seçilirse
+                    {
+                        katsayı1 = 1;
+                        katsayı2 = 1;
+                    }
+                    else    // hesaplama türü Sorensen-dice seçilirse
+                    {
+                        katsayı1 = 0.5;
+                        katsayı2 = 0.5;
+                    }
+
+                    double formul = kesisimsayı / (kesisimsayı + (katsayı1 * f1) + (katsayı2 * f2));    // formülü hesaplar
+
+                    MessageBox.Show("Girilen Metinler Arasındaki Benzerlik Oranı = " + formul.ToString());   // sonucu ekrana yazar
+                }
+            }
+                
+        }
+    }
+}
